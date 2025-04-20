932fdea0cd60278e06bf97cdc7400bf3f0c19123
Peter-Kisselev <peter.kisselev@gmail.com>
2025-04-20 01:10:43 -0400

    10x speed improvement

--- flood/submission.py
+++ flood/submission.py
@@ -10,20 +10,19 @@ class SubmissionBot(Bot):
 
     ### PASTE BELOW THIS LINE TO AVOID DEBUG ERRORS ###
 
+    # Constants
     VIEW = 8
+    VIEW_FULL = 17
     GRID_SIZE = 512
     DEBUG_INDEX = 5
-
     UNSEEN = 1 if DEBUG else -10000
 
+    # Parameters (tweak by hand) but constant at runtime
     EXPLORE = 300
     ESCAPE_MAX = 30
-
-    HIST_LEN = 2
-
+    HIST_LEN = 3
     MIN_GRAD = 0.5
-
-    MOMENTUM_MAX = 4
+    MOMENTUM_MAX = 2
 
     def rTF(self) -> bool:
         return random.choice([True, False])
@@ -66,18 +65,12 @@ class SubmissionBot(Bot):
 
         self.index = index
         self.difficulty = difficulty
-
         self.cache = np.full((self.GRID_SIZE, self.GRID_SIZE), self.UNSEEN, dtype=float)
         self.pos = np.array([0, 0])
-
         self.bestPos = [[0, 0], self.UNSEEN]
-
         self.TURN = 0
-
         self.ESCAPE = 0
-
-        self.posHist = [[0,0]]
-
+        self.posHist = [[0,0],[0,0]]
         self.momentum = 0
 
         # pick one direction based on index
@@ -86,13 +79,13 @@ class SubmissionBot(Bot):
 
     # wraparound
     def getTruePos(self, pos) -> np.ndarray[int, int]:
-        return np.array(pos)%self.GRID_SIZE
+        return pos%self.GRID_SIZE
 
     # Relative position of points on torus grid given an "origin"
     def torusRelPos(self, origin, pos):
         dx = ((pos[1] - origin[1] + self.GRID_SIZE//2) % self.GRID_SIZE) - self.GRID_SIZE//2
         dy = ((pos[0] - origin[0] + self.GRID_SIZE//2) % self.GRID_SIZE) - self.GRID_SIZE//2
-        return [dy, dx]
+        return np.array([dy, dx])
 
     # Pack binary message
     def packMsg(self, bestPos) -> int:
@@ -131,31 +124,71 @@ class SubmissionBot(Bot):
         a = self.decodeSigned(a_str)
         b = self.decodeSigned(b_str)
         c = int(c_str, 2)
-        return a, b, c
+        return [a, b, c]
 
-    # update "map"
+    # Avoid hitting known values, get only new positions (n^2 -> 2n complexity)
+    def slidingWindow(self, pos, dx, dy):
+        half = self.VIEW
+        x0_new = pos[1] - half  # cache origin x
+        y0_new = pos[0] - half  # cache origin y
+        bands = []
+
+        if dy != 0:
+            if dy > 0:
+                y_edge = y0_new + self.VIEW_FULL - 1  # Bottom edge of new view
+            else:
+                y_edge = y0_new  # Top edge of new view
+            x_vals = np.arange(x0_new, x0_new + self.VIEW_FULL)
+            local_y = y_edge - y0_new
+            local_x = x_vals - x0_new
+            row = np.stack((np.full(self.VIEW_FULL, y_edge), x_vals, np.full(self.VIEW_FULL, local_y), local_x), axis=1)
+            bands.append(row)
+
+        if dx != 0:
+            if dx > 0:
+                x_edge = x0_new + self.VIEW_FULL - 1  # Right edge of new view
+            else:
+                x_edge = x0_new  # Left edge of new view
+            y_vals = np.arange(y0_new, y0_new + self.VIEW_FULL)
+            local_x = x_edge - x0_new
+            local_y = y_vals - y0_new
+            col = np.stack((y_vals, np.full(self.VIEW_FULL, x_edge), local_y, np.full(self.VIEW_FULL, local_x)), axis=1)
+            bands.append(col)
+
+        if not bands:
+            return np.empty((0, 4), dtype=int)
+        # Stack as [cache_y, cache_x, local_y, local_x]
+        all_cells = np.vstack(bands)
+        return all_cells # allow duplicated corner when moving diagonally, performance cost is small
+
+    # Update known values with new things and update best position
     def updateCache(self, heights):
-        for i in range(len(heights)):
-            for j in range(len(heights[0])):
-                curPos = self.getTruePos([i - self.VIEW + self.pos[0], j - self.VIEW + self.pos[1]])
-                # if self.cache[tuple(curPos)] != heights[i][j] and self.cache[tuple(curPos)] != self.UNSEEN:
-                #     print(self.cache[tuple(curPos)])
-                #     print(heights[i][j])
-                self.cache[curPos[0]][curPos[1]] = heights[i][j]
-        # if self.DEBUG and self.index == self.DEBUG_INDEX:
-        #     # self.cPrint(self.pos)
-        #     self.saveCache(self.cache)
+        for val in self.slidingWindow(self.pos, self.movex, self.movey):
+            cache_pos = val[:2]
+            local_y = val[2]
+            local_x = val[3]
+
+            # if (0 <= local_y < self.VIEW_FULL) and (0 <= local_x < self.VIEW_FULL):
+            h = heights[local_y][local_x]
+            curPos = self.getTruePos(cache_pos)
+            self.cache[curPos[0]][curPos[1]] = h
+            if h > self.bestPos[1]:
+                self.bestPos = [curPos.copy(), h]
 
     # Use neighbor info
     def readNbrs(self, nbrs):
         for nbr in nbrs:
             rPos = [nbr[1], nbr[0]]
             msg = self.unpackMsg(nbr[2])
-
-            bP = [self.pos[0] + rPos[0] + msg[0], self.pos[1] + rPos[1] + msg[1]]
-            bH = msg[2]
+            bH = msg.pop(2)
+            # bP = [self.pos[0] + rPos[0] + msg[0], self.pos[1] + rPos[1] + msg[1]]
+            bP = np.add(self.pos, np.add(rPos, msg))
             newP = self.getTruePos(bP)
-            self.cache[newP[0]][newP[1]] = bH
+
+            if self.cache[newP[0]][newP[1]] == self.UNSEEN:
+                self.cache[newP[0]][newP[1]] = bH
+                if bH > self.bestPos[1]:
+                    self.bestPos = [newP.copy(), bH]
 
     # Compute gradient based on 3x3 grid centered on current position (for continuous section)
     def contGrad(self, heights):
@@ -169,21 +202,12 @@ class SubmissionBot(Bot):
         height = height.transpose()
         sign = lambda x: -1 if x < 0 else (0 if x == 0 else 1)
 
-        # if self.index == self.DEBUG_INDEX:
-        #     self.cPrint("test")
-
         self.updateCache(height)
         self.readNbrs(neighbors)
 
-        # Index of highest point in field of view
-        bP = np.unravel_index(np.argmax(self.cache), self.cache.shape)
-        if self.cache[bP[0]][bP[1]] > self.bestPos[1]:
-            self.bestPos = [bP, self.cache[bP[0]][bP[1]]]
-        bP = self.bestPos[0]
-
         if self.TURN > self.EXPLORE:
             if self.bestPos[1] > self.cache[self.pos[0]][self.pos[1]]:
-                dy, dx = self.torusRelPos(self.pos, bP)
+                dy, dx = self.torusRelPos(self.pos, self.bestPos[0])
 
                 movex = sign(dx)
                 movey = sign(dy)
@@ -204,6 +228,7 @@ class SubmissionBot(Bot):
                 if len(self.posHist) > self.HIST_LEN:
                     self.posHist.pop(0)
                 self.cPrint(self.posHist)
+                self.cPrint(self.pos)
 
                 grad = self.contGrad(height)
                 if (sum(grad**2) > self.MIN_GRAD) and (any(self.pos[i] != self.posHist[0][i] for i in range(len(self.pos)))):
